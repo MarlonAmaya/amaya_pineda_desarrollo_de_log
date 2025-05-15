50d7e57463777ab2d287862416ac140961a39706
Marlon Javier Amaya Pineda <amayapinedajavier@gmail.com>
2025-05-14 20:03:19 -0600

    FINALIZACION DE CREACION DE LOG

---
+++ core/secure/process.php
@@ -0,0 +1,64 @@
+<?php
+ini_set('display_error',1);
+ini_set('display_startup_error',1);
+include('inc/funciones.inc.php');
+include('secure/ips.php');
+
+$metodo_permitido = "POST";
+$archivo = "../logs/log.log";
+$dominio_autorizado = "localhost";
+$ip = ip_in_ranges($_SERVER["REMOTE_ADDR"],$rango);
+$txt_usuario_autorizado = "admin";
+$txt_password_autorizado = "admin";
+
+if(array_key_exists("HTTP_REFERER",$_SERVER)){
+    if(strpos($_SERVER["HTTP_REFERER"],$dominio_autorizado)){
+        if($ip == true){
+            if($_SERVER["REQUEST_METHOD"] == $metodo_permitido){
+                $valor_campo_usuario = ((array_key_exists("txt_user",$_POST))? htmlspecialchars(stripslashes(trim($_POST["txt_user"])),ENT_QUOTES) : "");
+                $valor_campo_password = ((array_key_exists("txt_pass",$_POST))? htmlspecialchars(stripslashes(trim($_POST["txt_pass"])),ENT_QUOTES) : "");
+                
+                if(($valor_campo_usuario != "" || strlen($valor_campo_usuario) > 0) && ($valor_campo_password != "" || strlen($valor_campo_password) > 0)){
+                    $usuario = preg_match('/^[a-zA-Z0-9]{1,10}+$/',$valor_campo_usuario);
+                    $password = preg_match('/^[a-zA-Z0-9]{1,10}+$/',$valor_campo_password);
+
+                    if($usuario !== false && $usuario !== 0 && $password !== false && $password !== 0){
+                        if($valor_campo_usuario === $txt_usuario_autorizado && $valor_campo_password === $txt_password_autorizado){
+                            echo("HOLA MUNDO");
+                            crear_editar_log($archivo,"EL CLIENTE INICIO SESION SATISFACTORIAMENTE",1,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+                        }else{
+                            crear_editar_log($archivo,"CREDEMCIALES INCORRECTAS ENVIADAS HACIA //$_SERVER[HTTP_HOST] $_SERVER[HTTP_REQUEST_URI]",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+                            header("HTTP/1.1 301 Moved Permanently");
+                            header("Location: ../?status=7");
+                        }
+                    }else{
+                        crear_editar_log($archivo,"ENVIO DE DATOS DEL FORMULARIO CON CARACTERES NO SOPORTADOS",3,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+                        header("HTTP/1.1 301 Moved Permanently");
+                        header("Location: ../?status=6");
+                    }
+                }else{
+                    crear_editar_log($archivo,"ENVIO DE CAMPOS VACIOS AL SERVIDOR",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+                    header("HTTP/1.1 301 Moved Permanently");
+                    header("Location: ../?status=5");
+                }   
+            }else{
+                crear_editar_log($archivo,"ENVIO DE METODO NO AUTORIZADO",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+                header("HTTP/1.1 301 Moved Permanently");
+                header("Location: ../?status=4");
+            }
+        }else{
+            crear_editar_log($archivo,"DIRECCION IP NO AUTORIZADA",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: ../?status=3");
+        }
+    }else{
+        crear_editar_log($archivo,"HA INTENTADO SUPLANTAR UN REFERER QUE NO ESTA AUTORIZADO",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+        header("HTTP/1.1 301 Moved Permanently");
+        header("Location: ../?status=2");
+    }
+}else {
+    crear_editar_log($archivo,"EL USUARIO HA INTENTADO INGRESAR AL SISTEMA DE UNA MANERA INCORRECTA",2,$_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"]);
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ../?status=1");
+}
+?>
